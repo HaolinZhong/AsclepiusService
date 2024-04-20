e3bdf4f06bdaae1ed42978b08061d3b8e5fe7ea6
HaolinZhong <zhlien1998@gmail.com>
2024-04-20 01:03:40 -0500

    add backend files

---
+++ src/main/java/com/asclepius/asclepiusservice/AsclepiusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.asclepius.asclepiusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsclepiusServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsclepiusServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asclepius/asclepiusservice/AsclepiusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asclepius.asclepiusservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsclepiusServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
